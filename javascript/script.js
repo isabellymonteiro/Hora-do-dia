344f3d1ce4b1bec54c68a17081d1f573ce24ec48
isabellymonteiro <isabellymr7@gmail.com>
2021-11-23 20:16:32 -0300

    change in files

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
 
-    var hora = 11 // data.getHours()
+    var hora = data.getHours()
     if (hora < 10) {
         hora = '0' + hora
     }
@@ -17,19 +17,19 @@ function carregar() {
 
     if (hora >= 0 && hora < 5) {
         // madrugada
-        img.src = 'madrugada.png'
+        img.src = 'imagens/madrugada.png'
         document.body.style.background = '#0f4158'
     } else if (hora >= 5 && hora < 12) {
         // bom dia
-        img.src = 'manha.png'
+        img.src = 'imagens/manha.png'
         document.body.style.background = '#d1ae89'
     } else if (hora >= 12 && hora < 18) {
         // boa tarde
-        img.src = 'tarde.png'
+        img.src = 'imagens/tarde.png'
         document.body.style.background = '#717b67'
     } else {
         // boa noite
-        img.src = 'noite.png'
+        img.src = 'imagens/noite.png'
         document.body.style.background = '#0a2427' 
     }
 }
